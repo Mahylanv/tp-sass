a14f225d677042165b78bdca0b4f1977769a12c6
Mahylan <mahylan.veclin@wangarden.com>
2024-02-26 16:55:20 +0100

    push final

--- sass-tp/src/App.js
+++ sass-tp/src/App.js
@@ -5,10 +5,14 @@ import TipsComponents from './components/pourboire';
 import ServiceComponents from './components/service';
 import RecapComponents from './components/checkbox/recap';
 import arrow from '../src/img/arrow.svg';
+import Header from './components/header';
+
 function App() {
   return (
       <header>
+        <Header />
         <CheckboxComponents></CheckboxComponents>
+        {/* Je n'ai pas réussi a mettre les liens entre les pages mon router-dom a pas marché il faut décommenter les components */}
         {/* <RecapComponents></RecapComponents> */}
         {/* <TipsComponents></TipsComponents> */}
         {/* <ServiceComponents></ServiceComponents> */}
